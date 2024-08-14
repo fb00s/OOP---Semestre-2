ae58be4d783c2c1fa93f9c706e8a3ff9db1bce02
fb00s <153862225+fb00s@users.noreply.github.com>
2024-08-14 20:58:01 -0300

    Add files via upload

---
+++ JavaApplication1/src/javaapplication1/Calculadora.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author felboos
+ */
+public class Calculadora {
+
+    double num1;
+    double num2;
+    
+    public void setNum1(double num1){
+        this.num1 = num1;
+    }
+    
+    public void setNum2(double num2){
+        this.num2 = num2;
+    }
+    
+    public double getNum1(){
+        return num1;
+    }
+    
+    public double getNum2(){
+        return num2;
+    }
+    
+    public double somar(){
+        return num1 + num2;
+    }
+    
+    public double subtrair(){
+        return num1 - num2;
+    }
+    
+    public double dividir(){
+        return num1 / num2;
+    }
+    
+    public double multiplicar(){
+        return num1 * num2;
+    }
+    
+}
