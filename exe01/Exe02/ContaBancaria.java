8bfbe66543a959bcad57ba7c62f9458d2152d760
fb00s <153862225+fb00s@users.noreply.github.com>
2024-08-21 21:34:01 -0300

    Add files via upload

---
+++ exe01/Exe02/ContaBancaria.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exe02;
+
+/**
+ *
+ * @author felboos
+ */
+public class ContaBancaria {
+    String numero;
+    String titular;
+    double saldo;
+    
+    public void setNumero(){
+        if (numero == null || )
+    }
+}
