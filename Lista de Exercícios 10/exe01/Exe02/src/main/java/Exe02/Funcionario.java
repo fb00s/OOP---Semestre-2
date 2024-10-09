b2dc0626cfa2f2f31ff1d21a7a0445b3598e635b
fb00s <153862225+fb00s@users.noreply.github.com>
2024-10-09 19:53:09 -0300

    Add files via upload

---
+++ Lista de Exercícios 10/exe01/Exe02/src/main/java/Exe02/Funcionario.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exe02;
+
+/**
+ *
+ * @author felboos
+ */
+public class Funcionario {
+    private String nome;
+    private double salarioBase;
+    
+    public Funcionario(String nome, double salarioBase){
+        setNome(nome);
+        setSalarioBase(salarioBase);
+    }
+    
+    public String getNome(){
+        return nome;
+    }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public double getSalarioBase(){
+        return salarioBase;
+    }
+    
+    public void setSalarioBase(double salarioBase){
+        this.salarioBase = salarioBase;
+    }
+    
+    public double calcularSalario(){
+        return salarioBase;
+    }
+    
+}
